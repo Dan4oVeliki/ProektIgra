f58661579e60b8adaa36fa1910a3722fb7f772b2
Dan4oVeliki <daani9000@gmail.com>
2024-02-07 11:13:23 +0200

    I MADE THE CHAIR WORK WOOO
    current issue is that the controller i just added does really work well, it spins one part of the chair forever if i try to turn

--- Assets/wheelController.cs
+++ Assets/wheelController.cs
@@ -1,29 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Content.Interaction;
 
 public class wheelController : MonoBehaviour
 {
-    private const string HORIZONTAL = "Horizontal";
-    private const string VERTICAL = "Vertical";
-    [SerializeField] WheelCollider wheelCollider;
-    private float HorizontalInput;
-    private float VerticalInput;
-    public float MotorSpeed;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public XRKnob knob; // Reference to your XR knob
+	public WheelCollider wheelCollider;
+	public float motorTorque = 1000f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        wheelCollider.motorTorque = HorizontalInput * MotorSpeed;
-    }
-    void GetInput()
-    {
-        HorizontalInput = Input.GetAxis(HORIZONTAL);
-        VerticalInput = Input.GetAxis(VERTICAL);
-    }
+	private float previousKnobValue = 0f;
+	private void Start()
+	{
+		previousKnobValue = knob.value;
+	}
+
+	private void Update()
+	{
+		// Assuming the XR knob value ranges from 0 to 1
+		float knobValue = knob.value;
+
+		// Check if the knob is turned forward (value increased)
+		if (knobValue > previousKnobValue)
+		{
+			// Apply motor torque only when turning forward
+			ApplyMotorTorque();
+		}
+
+		// Update the previous knob value for the next frame
+		previousKnobValue = knobValue;
+	}
+
+	private void ApplyMotorTorque()
+	{
+		// Apply custom motor torque to the wheel collider
+		wheelCollider.motorTorque = motorTorque;
+	}
 }
