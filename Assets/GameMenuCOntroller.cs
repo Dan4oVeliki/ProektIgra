308bba44595a9d7d14a77a9160fe4fdfcfc98b92
Dan4oVeliki <daani9000@gmail.com>
2024-02-26 01:08:50 +0200

    made the metro scene just visually, fixed wheelchair colliders, made the obsticle course

--- Assets/GameMenuCOntroller.cs
+++ Assets/GameMenuCOntroller.cs
@@ -3,21 +3,32 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.OpenXR.NativeTypes;
 
 public class GameMenuCOntroller : MonoBehaviour
 {
-    public GameObject menu;
-    public InputActionProperty showButton;
-    public Transform head;
-    public float MenuDistance;
-    private bool IsGamePaused = false;
-    void Start()
-    {
-        
-    }
+	public GameObject menu;
+	public InputActionProperty showButton;
+	public Transform head;
+	public float MenuDistance;
+	private bool IsGamePaused = false;
+	public GameObject LHand;
+	public GameObject RHand;
+	private LineRenderer lineRendererLHand; 
+	private LineRenderer lineRendererRHand; 
+	private XRInteractorLineVisual LHandRay;
+	private XRInteractorLineVisual RHandRay;
+	void Start()
+	{ 
+		lineRendererLHand = LHand.GetComponent<LineRenderer>();
+		lineRendererRHand = RHand.GetComponent<LineRenderer>();
+		LHandRay = LHand.GetComponent<XRInteractorLineVisual>();
+		RHandRay = RHand.GetComponent<XRInteractorLineVisual>();
+	}
 
-    // Update is called once per frame
-    void Update()
+	// Update is called once per frame
+	void Update()
 	{
 		if (showButton.action.WasPressedThisFrame())
 		{
@@ -34,11 +45,27 @@ public class GameMenuCOntroller : MonoBehaviour
 
 	public void OpenMenu()
 	{
-		
-			menu.SetActive(!menu.activeSelf);
+		menu.SetActive(!menu.activeSelf);
 
-			menu.transform.position = head.position + new Vector3(head.forward.x, 0f, head.forward.z).normalized * MenuDistance;
+		if (!menu.activeSelf)
+		{
+			// Disable the line renderers when the menu is active
+			LHandRay.enabled = false;
+			RHandRay.enabled = false;
+			lineRendererLHand.enabled = false;
+			lineRendererRHand.enabled = false;
+		}
+		else
+		{
+			// Enable the line renderers when the menu is not active
+			LHandRay.enabled = true;
+			RHandRay.enabled = true;
+			lineRendererLHand.enabled = true;
+			lineRendererRHand.enabled = true;
+		}
 
-			IsGamePaused = !IsGamePaused;
+		menu.transform.position = head.position + new Vector3(head.forward.x, 0f, head.forward.z).normalized * MenuDistance;
+
+		IsGamePaused = !IsGamePaused;
 	}
 }
